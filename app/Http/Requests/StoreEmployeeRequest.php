a27dd74dc407bb7fd3b87fdd3ea61919d8e1317e
VREEdom <35516476+vonsogt@users.noreply.github.com>
2022-04-05 18:20:45 +0700

    employee translation

--- app/Http/Requests/StoreEmployeeRequest.php
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -25,13 +25,42 @@ class StoreEmployeeRequest extends FormRequest
     {
         return [
             'name'                          => 'required|max:255',
-            'institution_number'            => 'required',
-            'id_card'                       => 'required',
-            'education_personnel_number'    => 'required',
+            'institution_number'            => 'required|unique:employees,institution_number',
+            'id_card'                       => 'required|unique:employees,id_card',
+            'education_personnel_number'    => 'required|unique:employees,education_personnel_number',
             'gender'                        => 'required',
             'join_date'                     => 'required',
             'institution_id'                => 'required',
             'position_id'                   => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Nama wajib diisi.',
+
+            'institution_number.required'   => 'Nomor induk yayasan wajib diisi.',
+            'institution_number.unique'     => 'Nomor induk yayasan sudah ada sebelumnya.',
+
+            'id_card.required'  => 'Nomor ktp wajib diisi.',
+            'id_card.unique'    => 'Nomor ktp sudah ada sebelumnya.',
+
+            'education_personnel_number.required'   => 'Nomor unik pendidik dan tenaga kependidikan wajib diisi.',
+            'education_personnel_number.unique'     => 'Nomor unik pendidik dan tenaga kependidikan sudah ada sebelumnya.',
+
+            'gender.required'   => 'Jenis kelamin wajib diisi.',
+
+            'join_date.required'   => 'Tanggal bergabung wajib diisi.',
+
+            'institution_id.required'   => 'Lembaga wajib diisi.',
+
+            'position_id.required'   => 'Posisi wajib diisi.',
         ];
     }
 }
--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -23,15 +23,46 @@ class UpdateEmployeeRequest extends FormRequest
      */
     public function rules()
     {
+        $employeeId = $this->employee->id;
+
         return [
             'name'                          => 'required|max:255',
-            'institution_number'            => 'required',
-            'id_card'                       => 'required',
-            'education_personnel_number'    => 'required',
+            'institution_number'            => 'required|unique:employees,institution_number,' . $employeeId,
+            'id_card'                       => 'required|unique:employees,id_card,' . $employeeId,
+            'education_personnel_number'    => 'required|unique:employees,education_personnel_number,' . $employeeId,
             'gender'                        => 'required',
             'join_date'                     => 'required',
             'institution_id'                => 'required',
             'position_id'                   => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Nama wajib diisi.',
+
+            'institution_number.required'   => 'Nomor induk yayasan wajib diisi.',
+            'institution_number.unique'     => 'Nomor induk yayasan sudah ada sebelumnya.',
+
+            'id_card.required'  => 'Nomor ktp wajib diisi.',
+            'id_card.unique'    => 'Nomor ktp sudah ada sebelumnya.',
+
+            'education_personnel_number.required'   => 'Nomor unik pendidik dan tenaga kependidikan wajib diisi.',
+            'education_personnel_number.unique'     => 'Nomor unik pendidik dan tenaga kependidikan sudah ada sebelumnya.',
+
+            'gender.required'   => 'Jenis kelamin wajib diisi.',
+
+            'join_date.required'   => 'Tanggal bergabung wajib diisi.',
+
+            'institution_id.required'   => 'Lembaga wajib diisi.',
+
+            'position_id.required'   => 'Posisi wajib diisi.',
         ];
     }
 }
