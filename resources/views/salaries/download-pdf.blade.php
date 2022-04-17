4500b51ded433bb1643edb6b4ff95b2d7d1931d3
VREEdom <35516476+vonsogt@users.noreply.github.com>
2022-04-17 12:31:45 +0700

    wip

--- resources/views/salaries/download-pdf.blade.php
+++ resources/views/salaries/download-pdf.blade.php
@@ -8,12 +8,12 @@
     <script src="{{ asset('assets/js/layout.min.js') }}"></script>
     <!-- Bootstrap Css -->
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    {{-- <!-- Icons Css -->
+    <!-- Icons Css -->
     <link href="{{ URL::asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
     <link href="{{ URL::asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
     <!-- custom Css-->
-    <link href="{{ URL::asset('assets/css/custom.min.css') }}" id="app-style" rel="stylesheet" type="text/css" /> --}}
+    <link href="{{ URL::asset('assets/css/custom.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 </head>
 <body>
 
