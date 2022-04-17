3ba7bb827f8371fad5edd503da883826725c0cdd
VREEdom <35516476+vonsogt@users.noreply.github.com>
2022-04-17 12:22:25 +0700

    wip

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,8 +9,8 @@
 
     <title>@yield('title')| HRIS Yayasan Darussalam Batam</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="Human Resource Information System Yayasan Darussalam Batam" name="description" />
+    <meta content="Anggi Syah Putri Nasution" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico')}}">
     @include('layouts.head-css')
