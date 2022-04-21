7fc27c9e348228f26daf9c73c818f927e96412e6
VREEdom <35516476+vonsogt@users.noreply.github.com>
2022-04-21 13:21:17 +0700

    exception page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.master-without-nav')
 
 @section('title')
-    404
+    503
 @endsection
 
 @section('body')
 
-<body>
-@endsection
-@section('content')
+    <body>
+    @endsection
+    @section('content')
         <div class="auth-page-wrapper pt-5">
             <!-- auth page bg -->
-            <div class="auth-one-bg-position auth-one-bg"  id="auth-particles">
+            <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
                 <div class="bg-overlay"></div>
 
                 <div class="shape">
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        viewBox="0 0 1440 120">
                         <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
                     </svg>
                 </div>
@@ -26,15 +27,21 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="text-center pt-4">
-                                <div class="">
-                                    <img src="{{ URL::asset('assets/images/error.svg') }}" alt="" class="error-basic-img move-animation">
+                            <div class="text-center mt-sm-5 pt-4">
+                                <div class="mb-5 text-white-50">
+                                    <h1 class="display-5 coming-soon-text">Situs sedang dalam Pemeliharaan</h1>
+                                    <p class="fs-14">Silakan periksa kembali nanti</p>
+                                    <div class="mt-4 pt-2">
+                                        <a href="/" class="btn btn-success"><i class="mdi mdi-home me-1"></i> Kembali ke beranda</a>
+                                    </div>
                                 </div>
-                                <div class="mt-n4">
-                                    <h1 class="display-1 fw-medium">404</h1>
-                                    <h3 class="text-uppercase">Maaf, Halaman tidak Ditemukan 😭</h3>
-                                    <p class="text-muted mb-4">Halaman yang Anda cari tidak tersedia!</p>
-                                    <a href="/" class="btn btn-success"><i class="mdi mdi-home me-1"></i>Kembali ke beranda</a>
+                                <div class="row justify-content-center mb-5">
+                                    <div class="col-xl-4 col-lg-8">
+                                        <div>
+                                            <img src="{{ URL::asset('assets/images/maintenance.png') }}" alt=""
+                                                class="img-fluid">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -62,11 +69,8 @@
 
         </div>
         <!-- end auth-page-wrapper -->
-
-@endsection
-@section('script')
-<!-- particles js -->
-<script src="{{ URL::asset('assets/libs/particles.js/particles.js.min.js') }}"></script>
-<!-- particles app js -->
-<script src="{{ URL::asset('assets/js/pages/particles.app.js') }}"></script>
-@endsection
+    @endsection
+    @section('script')
+        <script src="{{ URL::asset('assets/libs/particles.js/particles.js.min.js') }}"></script>
+        <script src="{{ URL::asset('assets/js/pages/particles.app.js') }}"></script>
+    @endsection
