fb26b7d45b01245161c705c9cc87593fc60361f2
VREEdom <35516476+vonsogt@users.noreply.github.com>
2022-05-24 12:42:48 +0700

    fix enum description

--- app/Enums/Education.php
+++ app/Enums/Education.php
@@ -3,20 +3,22 @@
 namespace App\Enums;
 
 use BenSampo\Enum\Enum;
+use BenSampo\Enum\Attributes\Description;
 
 /**
  * @method static static SD()
  * @method static static SMP()
- * @method static static SMA()
+ * @method static static SMA_SMK()
  * @method static static D3()
-* @method static static S1()
+ * @method static static S1()
  * @method static static S2()
  */
 final class Education extends Enum
 {
     const SD =      1;
     const SMP =     2;
-    const SMA =     3;
+    #[Description('SMA/SMK')]
+    const SMA_SMK = 3;
     const D3 =      4;
     const S1 =      5;
     const S2 =      6;
