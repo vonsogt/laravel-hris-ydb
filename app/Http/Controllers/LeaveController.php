d9b1439c192e4daf5123c6e4300dba4f67390908
Vonso FH <vonsofh@gmail.com>
2023-01-30 19:32:36 +0700

    wip

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -63,22 +63,30 @@ class LeaveController extends Controller
                 })
                 ->addIndexColumn()
                 ->addColumn('is_approved', function ($row) use ($type) {
-                    if ($type == 'approve' && auth()->getDefaultDriver() == 'api' && auth()->user()->position->name == 'Kepala HRD') {
-
+                    if (auth()->getDefaultDriver() == 'api' && auth()->user()->position->name == 'Kepala HRD') {
                         $start_date = Carbon::make($row->start_date)->format('d M Y');
                         $end_date = Carbon::make($row->end_date)->format('d M Y');
 
                         return '<a href="javascript:void(0)" class="btn btn-sm btn-danger" onclick="updateEntry(this)" data-route="' . route("leaves.approve", $row->id) . '" data-value="0" data-employee-name="' . $row->employee->name . '" data-start-date="' . $row->start_date . '" data-end-date="' . $row->end_date . '">Tolak</a> ' .
                             '<a href="javascript:void(0)" class="btn btn-sm btn-success" onclick="updateEntry(this)" data-route="' . route("leaves.approve", $row->id) . '" data-value="1" data-employee-name="' . $row->employee->name . '" data-start-date="' . $start_date . '" data-end-date="' . $end_date . '">Setujui</a>';
                     } else {
-                        if ($row->is_approved !== null) {
-                            if ($row->is_approved == 1) {
-                                return '<button type="button" class="btn btn-success">Disetujui</button>';
-                            } else if ($row->is_approved == 0) {
-                                return '<button type="button" class="btn btn-danger">Ditolak</button>';
+                        if ($type == 'approve') {
+
+                            $start_date = Carbon::make($row->start_date)->format('d M Y');
+                            $end_date = Carbon::make($row->end_date)->format('d M Y');
+
+                            return '<a href="javascript:void(0)" class="btn btn-sm btn-danger" onclick="updateEntry(this)" data-route="' . route("leaves.approve", $row->id) . '" data-value="0" data-employee-name="' . $row->employee->name . '" data-start-date="' . $row->start_date . '" data-end-date="' . $row->end_date . '">Tolak</a> ' .
+                                '<a href="javascript:void(0)" class="btn btn-sm btn-success" onclick="updateEntry(this)" data-route="' . route("leaves.approve", $row->id) . '" data-value="1" data-employee-name="' . $row->employee->name . '" data-start-date="' . $start_date . '" data-end-date="' . $end_date . '">Setujui</a>';
+                        } else {
+                            if ($row->is_approved !== null) {
+                                if ($row->is_approved == 1) {
+                                    return '<button type="button" class="btn btn-success">Disetujui</button>';
+                                } else if ($row->is_approved == 0) {
+                                    return '<button type="button" class="btn btn-danger">Ditolak</button>';
+                                }
                             }
+                            return '<button type="button" class="btn btn-success">Menunggu Approval</button>';
                         }
-                        return '<button type="button" class="btn btn-success">Menunggu Approval</button>';
                     }
                 })
                 ->addColumn('employee_name', function (Leave $leave) {
